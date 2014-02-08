1754c355f105d311b437239e410c66999e526e39
Tim Gibbon <tim@mylook.ltd.uk>
2014-02-08 11:01:12 +0000

    Added new looping, talking

--- scripts/look_whos_talking_two.py
+++ scripts/look_whos_talking_two.py
@@ -1,5 +1,6 @@
 import mcpi.minecraft as minecraft
 mc = minecraft.Minecraft.create()
+print ("Hold down Ctrl and Press C to stop")
 while True:
     message = raw_input("Write here to chat:")
     mc.postToChat(message)
