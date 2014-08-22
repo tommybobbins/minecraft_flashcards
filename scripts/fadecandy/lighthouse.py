4b40c6ca8bf73b58ddbc4ec68036d25d638af26b
Tim Gibbon <tng@chegwin.org>
2014-08-22 21:23:09 +0100

    Moved to library

--- scripts/fadecandy/lighthouse.py
+++ scripts/fadecandy/lighthouse.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
+import sys
 import mcpi.minecraft as minecraft
-import pygame
-pygame.init()
 import mcpi.block as block
 mc = minecraft.Minecraft.create()
 from time import sleep
@@ -11,10 +10,12 @@ import thread
 found_lighthouses = 0
 lighthouse = 0
 lighthouses={}
+colourmap = {14 : "red", 13 : "green", 11 : "blue" }
 lighthousered=0
 lighthousegreen=0
 lighthouseblue=0
-woolcolours = ['red', 'green', 'blue']
+sys.path.append("/home/pi/minecraft_flashcards/scripts/library/")
+from lighthouse_setup import create_lighthouse,destroy_lighthouse
 numLEDs = 16
 brightness = 256
 pulse_time = 0.04
@@ -26,16 +27,7 @@ leds_roneback=(7,8,9,10,11,12,13,14,15,0,1,2,3,4,5,6)
 pixels = [ (0,0,0) ] * numLEDs
 
 ##############################################################
-# Balance board code 
-# Initialize the joysticks
-pygame.joystick.init()
-joystick = pygame.joystick.Joystick(0)
-joystick.init()
 bb_pos={}
-name = joystick.get_name()
-print("Joystick name: {%s}" % name )
-axes = joystick.get_numaxes()
-print("Number of axes: {%s}" % axes )
 # Average 8 year old
 #calibration_factor = 1.02
 # Average Adult
@@ -44,16 +36,10 @@ calibration_factor = 1.05
 ##### Make the game easier with high number_of_lighthouses_make
 ##### compared to number_of_lighthouses_find
 number_of_lighthouses_find = 12
-number_of_lighthouses_make = 60
+number_of_lighthouses_make = 16
 map_sizea = 55 
 map_sizeb = 100
-fourthreethree = False
 espeakEnabled=False
-###############################################
-###Uncomment if there is a 433 Transmitter
-#from fourthreethree_transmitter.threeon import switch_socket
-#fourthreethree = True
-###############################################
 ###############################################
 # To enable the game to speak to the player:
 # $ sudo apt-get install python-espeak
@@ -62,24 +48,6 @@ from espeak import espeak
 espeakEnabled=True
 ###############################################
 import random
-def create_lighthouse(x,z,colour):
-    # Create a lighthouse at x,z 
-    # Red = 14, Blue = 11, Green = 13
-    if (colour == "red"):
-       woolcolour = 14
-    elif (colour == "blue"):
-       woolcolour = 11
-    elif (colour == "green"):
-       woolcolour = 13
-    else:
-       woolcolour = 14
-    height = mc.getHeight(x,z)
-    mc.setBlock(x, height, z , block.WOOL.id, 0 )
-    mc.setBlock(x, height+1, z , block.WOOL.id, woolcolour )
-    mc.setBlock(x, height+2, z , block.WOOL.id, 0 )
-    mc.setBlock(x, height+3, z , block.WOOL.id, woolcolour )
-    mc.setBlock(x, height+4, z , 20 )
-    return(x,height,z)
 
 def light_neopixelring(colours,rotations):
     pixels = [ (0,0,0) ] * numLEDs
@@ -115,18 +83,12 @@ def light_lighthouse(colour,lighthousered,lighthousegreen,lighthouseblue):
     mc.postToChat("Current colour = %i,%i,%i " % (lighthousered, lighthousegreen, lighthouseblue))
     return (lighthousered,lighthousegreen, lighthouseblue)
 
-def destroy_lighthouse(x,y,z):
-    height = mc.getHeight(x,z)
-    height = y
-    mc.setBlocks(x, height, z ,x , height+4, z, block.AIR.id, 0 )
-#    mc.postToChat("Cleaning up lighthouse %i %i %i" % (x,y,z))
-
 if __name__ == "__main__":
         # Build initial set of lighthouses at random positions on the map
     while (lighthouse < number_of_lighthouses_make):
         xlighthouse=random.randint(-map_sizeb,map_sizea)
         zlighthouse=random.randint(-map_sizea,map_sizeb)
-        random_colour=random.choice(woolcolours)
+        random_colour=random.choice(colourmap.keys())
         lighthouses[lighthouse]=create_lighthouse(xlighthouse,zlighthouse,random_colour)
 #        mc.postToChat("Created lighthouse %i" % lighthouse)
         lighthouse += 1
@@ -141,35 +103,6 @@ if __name__ == "__main__":
     start_game = time.time()
     while (found_lighthouses < number_of_lighthouses_find):
         pos = mc.player.getTilePos()
-        direction_of_travel = "stopped"
-        events=pygame.event.get()
-        for i in range( axes ):
-            bb_pos[i] = float(joystick.get_axis(i))
-        forwards_or_backwards = (bb_pos[2] + bb_pos[0])/(bb_pos[3] + bb_pos[1])
-        left_or_right = (bb_pos[2] + bb_pos[3])/(bb_pos[0] + bb_pos[1])
-        abs_fb = abs(forwards_or_backwards - 1.0)
-        abs_lr = abs(left_or_right - 1.0)
-        if abs_fb > abs_lr:
-            print ("Mainly trying to move forward backwards")
-            if forwards_or_backwards < 1.0/calibration_factor:
-                direction_of_travel="Forwards"
-                mc.player.setTilePos(pos.x,pos.y,pos.z+1)
-            elif forwards_or_backwards > 1.0*calibration_factor:
-                direction_of_travel="Backwards"
-                mc.player.setTilePos(pos.x,pos.y,pos.z-1)
-            else:
-                direction_of_travel="Stopped"
-        elif abs_fb < abs_lr:
-            print ("Mainly trying to move Left Right")
-            if left_or_right < 1.0/calibration_factor:
-                direction_of_travel="Left"
-                mc.player.setTilePos(pos.x+1,pos.y,pos.z)
-            elif left_or_right > 1.0*calibration_factor:
-                direction_of_travel="Right"
-                mc.player.setTilePos(pos.x-1,pos.y,pos.z)
-            else:
-                direction_of_travel="Stopped"
-        print (direction_of_travel)
         blockBelow = mc.getBlock(pos.x, pos.y - 1, pos.z)
         if (blockBelow == 20):
             block2Below = mc.getBlockWithData(pos.x, pos.y - 2, pos.z)
@@ -179,10 +112,6 @@ if __name__ == "__main__":
             (lighthousered,lighthousegreen,lighthouseblue)=light_lighthouse(block2Below.data,lighthousered,lighthousegreen,lighthouseblue)
             colourtuple=(lighthousered,lighthousegreen,lighthouseblue)
             thread.start_new_thread(light_neopixelring,(colourtuple,2))
-            if (fourthreethree):
-                switch_socket('on')
-                sleep(1)
-                switch_socket('off')
             found_lighthouses += 1
             number_of_lighthouses_left = number_of_lighthouses_find - found_lighthouses
             mc.postToChat("Found %i lighthouses, %i to go" % (found_lighthouses,number_of_lighthouses_left))
@@ -201,5 +130,3 @@ if __name__ == "__main__":
         sleep(1)
         espeak.synth("Found all lighthouses.")
     mc.postToChat("Removed all lighthouses")
-    if (fourthreethree):
-        switch_socket('off')
