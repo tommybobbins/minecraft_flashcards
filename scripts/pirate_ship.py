71bd43ec7dd415efc03b160059dd3dc642df32c9
Tim Gibbon <tng@chegwin.org>
2014-08-24 11:42:13 +0100

    Tidied up fore section

--- scripts/pirate_ship.py
+++ scripts/pirate_ship.py
@@ -31,7 +31,7 @@ mc.setBlocks(pos.x + (a/2) - 1 , pos.y + height + 1  , pos.z + (b - 2), pos.x - 
 # Aft section
 mc.setBlocks(pos.x + (a - 1) , pos.y + height , pos.z + (b - 1), pos.x - (a - 1), pos.y + height, pos.z + (b - ((3*b)/4)) , 5)    
 # Forecastle
-mc.setBlocks(pos.x + (a - 2) , pos.y + height , pos.z - (b - 1), pos.x - (a - 2), pos.y + height, pos.z - (b - ((3*b)/4)) , 5)    
+mc.setBlocks(pos.x + (a - 2) , pos.y + height , pos.z - (b - 1), pos.x - (a - 2), pos.y + height, pos.z - (b - ((1*b)/2)) , 5)    
 # Doors windows etc
 # Glass
 mc.setBlocks(pos.x + (a/2 - 1),pos.y + height + 2, pos.z + (b/2), pos.x - (a/2 - 1), pos.y + height +2, pos.z + (b/2), 20 ) 
@@ -40,7 +40,7 @@ mc.setBlocks(pos.x + (a/2 - 1),pos.y + height + 1, pos.z + (b/2), pos.x - (a/2 -
 mc.setBlocks(pos.x,pos.y + height + 1, pos.z + (b/2),pos.x,pos.y + height + 2, pos.z + (b/2), 0 ) 
 mc.setBlock(pos.x,pos.y + height + 1, pos.z + (b/2), 64 ) 
 mc.setBlock(pos.x,pos.y + height + 2, pos.z + (b/2), 64 ) 
-# Wood slab used to detect when player is in ship
+# Nether reactor used to detect when player is in ship
 mc.setBlock(pos.x,pos.y + height, pos.z + (b/2) + 2, 247 ) 
 
 #### BOW SPIRIT #####################################
@@ -79,4 +79,5 @@ mc.setBlocks(pos.x + 2 , pos.y + 4 , pos.z , pos.x + 2, pos.y + (height + 12), p
 mc.setBlocks(pos.x + 3 , pos.y + 4 , pos.z , pos.x + 3, pos.y + (height + 12), pos.z , 35,0)    
 mc.setBlocks(pos.x + 4 , pos.y + 4 , pos.z , pos.x + 4, pos.y + (height + 12), pos.z , 35,14)    
 mc.setBlocks(pos.x + 5 , pos.y + 4 , pos.z , pos.x + 5, pos.y + (height + 12), pos.z , 35,0)    
-mc.player.setPos(pos.x , pos.y + height + 5, pos.z + 2 )    
+
+mc.player.setPos(pos.x,pos.y + height, pos.z + (b/2) + 2, 247 ) 
